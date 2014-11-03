59845ce96ea110580929df126cf14d662e4f4e08
Tim Williams <timothy.williams@cern.ch>
2014-11-03 15:48:40 +0000

    replaced using namespace std with std::

--- Input.cpp
+++ Input.cpp
@@ -1,48 +1,48 @@
 #include<iostream>
 #include<string>
-using namespace std;
+//using namespace std;
 
 int main(){
  double a;
  double b;
- string operation;
- string plus="+";
- string minus="-";
- string times="*";
- string divide="/";
+ std::string operation;
+ std::string plus="+";
+ std:: string minus="-";
+ std:: string times="*";
+ std:: string divide="/";
 
- cout<<"enter number 1"<<endl; //enter first number
- cin>>a;
+ std::cout<<"enter number 1"<<std::endl; //enter first number
+ std::cin>>a;
    //input validation
- if(!cin){
-   cout<<"incorrect input, stop trying to break my programme!"<<endl;
+ if(!std::cin){
+   std::cout<<"incorrect input, stop trying to break my programme!"<<std::endl;
  } 
 
- cout<<"enter number 2"<<endl; //enter second number
- cin>>b;
- if(!cin){  //input validation
-   cout<<"incorrect input, stop trying to break my programme!"<<endl; 
+ std::cout<<"enter number 2"<<std::endl; //enter second number
+ std::cin>>b;
+ if(!std::cin){  //input validation
+   std::cout<<"incorrect input, stop trying to break my programme!"<<std::endl; 
  }
  
- cout<<"enter operator"<<endl;
- cin>>operation;               //fill operator string;
+ std::cout<<"enter operator"<<std::endl;
+ std::cin>>operation;               //fill operator string;
  
  if(operation.compare(plus)==0){ //perform addition
-   cout<<" a+b= "<<a+b<<endl;
+   std::cout<<" a+b= "<<a+b<<std::endl;
  }
  if(operation.compare(minus)==0){ //perform subtraction
-    cout<<" a-b= "<<a-b<<endl;
+    std::cout<<" a-b= "<<a-b<<std::endl;
  }
  if(operation.compare(times)==0){  //perform times
-   cout<<" a*b ="<<a*b<<endl;
+   std::cout<<" a*b ="<<a*b<<std::endl;
  }
  if(operation.compare(divide)==0){ //peform divide
-   cout<<" a/b ="<<a/b<<endl;
+   std::cout<<" a/b ="<<a/b<<std::endl;
  }
  if((operation.compare(plus)!=0)&&(operation.compare(minus)!=0)&&(operation.compare(times)!=0)&&(operation.compare(divide)!=0)){
-   cout<<"a=  "<<a<<"  b=   "<<b<<endl;  //default option if operator incorrect
+   std::cout<<"a=  "<<a<<"  b=   "<<b<<std::endl;  //default option if operator incorrect
  }
 
- //cout<<"product is=  "<<a*b<<endl; //cout product
+ //std::cout<<"product is=  "<<a*b<<std::endl; //std::cout product
  return 0;
 }
