a71e59898e9829c5eefcccd8633127fc80862d3b
Tim Williams <timothy.williams@cern.ch>
2014-11-03 15:42:38 +0000

    Added default option in case no operator is specified

--- Input.cpp
+++ Input.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(){
-double a;
-double b;
+ double a;
+ double b;
  string operation;
  string plus="+";
  string minus="-";
@@ -21,7 +21,7 @@ double b;
  cout<<"enter number 2"<<endl; //enter second number
  cin>>b;
  if(!cin){  //input validation
- cout<<"incorrect input, stop trying to break my programme!"<<endl; 
+   cout<<"incorrect input, stop trying to break my programme!"<<endl; 
  }
  
  cout<<"enter operator"<<endl;
@@ -39,8 +39,9 @@ double b;
  if(operation.compare(divide)==0){ //peform divide
    cout<<" a/b ="<<a/b<<endl;
  }
-
-   
+ if((operation.compare(plus)!=0)&&(operation.compare(minus)!=0)&&(operation.compare(times)!=0)&&(operation.compare(divide)!=0)){
+   cout<<"a=  "<<a<<"  b=   "<<b<<endl;  //default option if operator incorrect
+ }
 
  //cout<<"product is=  "<<a*b<<endl; //cout product
  return 0;
