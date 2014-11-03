d85934cc5e082b366886d8e16adb02b05e98f3fe
Tim Williams <timothy.williams@cern.ch>
2014-11-03 14:42:15 +0000

    added input.cpp which takes 2 input numbers and multiplys together

---
+++ Input.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+double a;
+double b;
+
+ cout<<"enter number 1"<<endl;
+ cin>>a;
+ cout<<"enter number 2"<<endl;
+ cin>>b;
+
+ cout<<"product is=  "<<a*b<<endl;
+ return 0;
+}
